444e7b8c81314b8a505c0bca5de62bc11ce9925d
tpatwary <tanvirpatwary45@gmail.com>
2023-10-29 18:00:02 -0400

    Projet created by Tanu

---
+++ src/test/java/com/ny/javabasic/Methods_types.java
@@ -0,0 +1,28 @@
+package com.ny.javabasic;
+
+public class Methods_types {
+
+	public void getSalary() {// void method> return nothing
+
+	}
+
+	String getEmpName() {// return method> return something
+		return null;
+
+	}
+
+	public void getEmpInfo(String name) {// void parameter method
+
+	}
+	
+	String getage() {
+		return null;
+		
+		
+	}
+	double getmoney() {
+		return 0;
+		
+	}
+
+}
